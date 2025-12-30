b8c275e459851b61d16d70f66234eb689113ebd1
Abdelrhman Aboelfadl <abdelrhmanabofadlqassim111@gmail.com>
2025-12-30 21:11:37 +0200

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -16,3 +16,29 @@ function StopClose(BoxCalss) {
         e.stopPropagation();
     })
 }
+function slide() {
+    new Splide('#splide', {
+    type      : 'loop',
+    direction : 'rtl',
+
+    autoplay  : true,
+    speed     : 1000,
+    interval  : 2000,
+    gap       : '20px',
+
+    perPage   : 3,   // Desktop
+
+    breakpoints: {
+        1200: {
+        perPage: 2,
+        },
+        768: {
+        perPage: 1,
+        gap: '12px',
+        }
+    },
+
+pauseOnHover: true,
+}).mount();
+
+}
--- js/index.js
+++ js/index.js
@@ -1,15 +1,1 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:4,
-    loop:true,
-    margin:30,
-    autoplay:true,
-    autoplayTimeout:1600,
-    autoplayHoverPause:true
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
+slide();
